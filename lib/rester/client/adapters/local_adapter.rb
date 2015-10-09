164c4634830d258251d36f6f5dd654062a4eccdc
Robert Honer <robert@ribbon.co>
2015-10-09 10:40:52 -0700

    [#4] Adding LocalAdapter

---
+++ lib/rester/client/adapters/local_adapter.rb
@@ -0,0 +1,50 @@
+require 'stringio'
+
+module Rester
+  module Client::Adapters
+    ##
+    # An adapter for "connecting" to a service internally, without needing to
+    # interface over a HTTP connection.
+    class LocalAdapter < Adapter
+      attr_reader :service
+
+      def connect(service)
+        nil.tap { @service = service }
+      end
+
+      def connected?
+        !!service
+      end
+
+      def get!(path, params={})
+        _request(:get, path, headers: headers, query: params)
+      end
+
+      def delete!(path, params={})
+        _request(:delete, path, headers: headers, query: params)
+      end
+
+      def put!(path, params={})
+        _request(:put, path, headers: headers, data: params)
+      end
+
+      def post!(path, params={})
+        _request(:post, path, headers: headers, data: params)
+      end
+
+      private
+
+      def _request(verb, path, opts={})
+        body = URI.encode_www_form(opts[:data] || {})
+        query = URI.encode_www_form(opts[:query] || {})
+
+        service.call(
+          'REQUEST_METHOD' => verb.to_s.upcase,
+          'PATH_INFO'      => path,
+          'QUERY_STRING'   => query,
+          'rack.input'     => StringIO.new(body)
+        ).tap { |r| r.delete_at(1) }
+      end
+    end # LocalAdapter
+  end # Client::Adapters
+end # Rester
