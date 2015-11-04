479e864ce26c2ad02c076a37d3a051a28fa9cb91
Kayvon Ghaffari <kayvon@ribbon.co>
2015-11-04 12:04:48 -0800

    [#28] Improve tests

--- spec/rester/rspec_spec.rb
+++ spec/rester/rspec_spec.rb
@@ -5,16 +5,19 @@ RSpec.describe Rester::DummyService, rester: 'spec/stubs/dummy_service_stub.yml'
     context 'GET' do
       context 'With some context' do
         it { is_expected.to eq stub_response }
+        it { is_expected.to eq [200, '{"string":"some_string","integer":1,"float":1.23,"symbol":"some_symbol","bool":true,"method":"search"}'] }
       end
 
       context 'With bad param' do
         it { is_expected.to eq stub_response }
+        it { is_expected.to eq [400, '{"message":"integer failed between?(0,100) validation"}'] }
       end
     end # GET
 
     context 'POST' do
       context 'With some context' do
         it { is_expected.to eq stub_response }
+        it { is_expected.to eq [201 , '{"string":"some_string","integer":1,"float":1.23,"symbol":"some_symbol","bool":true,"method":"create"}'] }
       end
     end # POST
   end # /v1/tests
@@ -23,18 +26,21 @@ RSpec.describe Rester::DummyService, rester: 'spec/stubs/dummy_service_stub.yml'
     context 'GET' do
       context 'With some context' do
         it { is_expected.to eq stub_response }
+        it { is_expected.to eq [200, '{"token":"abc123","params":{"string":"some_string","integer":1,"float":1.23,"symbol":"some_symbol","bool":true},"method":"get"}'] }
       end
     end # GET
 
     context 'PUT' do
       context 'With some context' do
         it { is_expected.to eq stub_response }
+        it { is_expected.to eq [200, '{"method":"update","int":1,"float":1.1,"bool":true,"null":null,"params":{"string":"some_string","integer":1,"float":1.23,"symbol":"some_symbol","bool":true,"test_token":"abc123"}}'] }
       end
     end # PUT
 
     context 'DELETE' do
       context 'With some context' do
         it { is_expected.to eq stub_response }
+        it { is_expected.to eq [200, '{"token":"abc123","params":{"string":"some_string","integer":1,"float":1.23,"symbol":"some_symbol","bool":true},"method":"delete"}'] }
       end
     end # DELETE
   end # /v1/tests/abc123
@@ -43,6 +49,7 @@ RSpec.describe Rester::DummyService, rester: 'spec/stubs/dummy_service_stub.yml'
     context 'GET' do
       context 'With some context' do
         it { is_expected.to eq stub_response }
+        it { is_expected.to eq [200, '{"some_param":"some_param","test_token":"abc123","method":"search"}'] }
       end
     end # GET
   end # /v1/tests/abc123/mounted_objects
