6b768319fdf48774dfdc9f4f1e14b18b1a0a15ab
Kayvon Ghaffari <kayvon@ribbon.co>
2015-11-04 17:02:00 -0800

    [#28] Add test suite completion validation

--- lib/rester/rspec.rb
+++ lib/rester/rspec.rb
@@ -11,6 +11,8 @@ RSpec.configure do |config|
       raise "invalid service to test"
     end
     @rester_adapter = Rester::Client::Adapters::LocalAdapter.new(klass, {})
+
+    _validate_test_coverage(ex)
   end
 
   config.before :each, rester: // do |ex|
@@ -45,7 +47,6 @@ RSpec.configure do |config|
         "Could not find path: #{path.inspect} verb: #{verb.inspect} context: #{context.inspect} in #{@rester_stub_filepath}"
     end
 
-
     ex.example_group.let(:subject) {
       @rester_adapter.request(verb.downcase.to_sym, path, params)
     }
@@ -53,5 +54,99 @@ RSpec.configure do |config|
     ex.example_group.let(:stub_response) {
       [response['code'], response['body'].to_json]
     }
+  end
+
+  config.after :each, rester: // do |ex|
+    expect(subject).to eq stub_response
+  end
+
+  ##
+  # Check to see if each stub example has a corresponding test written for it
+  def _validate_test_coverage(ex)
+    rester_service_tests = _rester_service_tests(ex)
+    missing_tests = _missing_stub_tests(rester_service_tests)
+    puts missing_tests.inspect
+
+    # Loop through each missing stub test and create a corresponding RSpect test
+    # to display the missing tests as a failure to the user
+    missing_tests.each { |missing_path, missing_verbs|
+      path_group = ex.class.children.find { |child_group|
+        child_group.description == missing_path
+      }
+      path_group = ex.class.describe(missing_path) if path_group.nil?
+
+      missing_verbs.each { |missing_verb, missing_contexts|
+        verb_group = path_group.children.find { |child_group|
+          child_group.description == missing_verb
+        }
+        verb_group = path_group.context(missing_verb) if verb_group.nil?
+
+        missing_contexts.each { |missing_context, _|
+          context_group = verb_group.children.find { |child_group|
+            child_group.description == missing_context
+          }
+          context_group = verb_group.context(missing_context) if context_group.nil?
+
+          context_group.it { is_expected.to eq stub_response }
+        }
+      }
+    }
+  end
+
+  def _rester_service_tests(parent_example_group)
+    service_tests = {}
+    parent_example_group.class.children.each { |path_group|
+      path = path_group.description
+      service_tests[path] ||= {}
+
+      path_group.children.each { |verb_group|
+        verb = verb_group.description
+        service_tests[path][verb] ||= {}
+
+        verb_group.children.each { |context_group|
+          context =  context_group.description
+          service_tests[path][verb][context] = context_group.examples.count > 0
+        }
+      }
+    }
+
+    service_tests
+  end
+
+  ##
+  # Takes a hash produced by _rester_service_tests.
+  # Returns a hash of only the missing stub tests:
+  #
+  # {
+  #   "/v1/tests/abc123/mounted_objects" => {
+  #     "POST" => {
+  #       "With some context" => false
+  #     }
+  #   },
+  #   "/v1/stuff" => {
+  #     "GET" => {
+  #       "Doing that" => false
+  #     }
+  #   }
+  # }
+  def _missing_stub_tests(tests)
+    @rester_stub.reject { |k, _|
+      ['version', 'consumer', 'producer'].include?(k)
+    }.map { |path, verbs|
+      [
+        path,
+        verbs.map { |verb, contexts|
+          [
+            verb,
+            contexts.map { |context, _|
+              [
+                context,
+                !!(tests[path] && tests[path][verb] && tests[path][verb][context])
+              ]
+            }.to_h.reject { |_, v| v }
+          ]
+        }.to_h.reject { |_, v| v.empty? }
+      ]
+    }.to_h.reject { |_, v| v.empty? }
   end
 end
