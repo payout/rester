701c7822b41bc901a316828ee9c3b05cd5d96ebb
Robert Honer <robert@ribbon.co>
2015-12-01 16:58:38 -0800

    [#22] Adding Client::Adapters.connect tests.

--- spec/rester/client/adapters_spec.rb
+++ spec/rester/client/adapters_spec.rb
@@ -16,6 +16,67 @@ module Rester
           )
         end
       end # ::list
+
+      describe '::connect', :connect do
+        let(:opts) { {} }
+        subject { Adapters.connect(service, opts) }
+
+        context 'with url' do
+          let(:service) { RSpec.server_uri.to_s }
+
+          it { is_expected.to be_a Rester::Client::Adapters::HttpAdapter }
+          it { is_expected.to have_attributes(connected?: true) }
+
+          it 'should be connected to correct url' do
+            expect(subject.connection.url.to_s).to eq service
+          end
+        end # with url
+
+        context 'with URI object' do
+          let(:service) { RSpec.server_uri }
+
+          it { is_expected.to be_a Rester::Client::Adapters::HttpAdapter }
+          it { is_expected.to have_attributes(connected?: true) }
+
+          it 'should be connected to correct url' do
+            expect(subject.connection.url).to eq service
+          end
+        end # with URI object
+
+        context 'with service class' do
+          let(:service) { DummyService }
+
+          it { is_expected.to be_a Rester::Client::Adapters::LocalAdapter }
+          it { is_expected.to have_attributes(connected?: true) }
+
+          it 'should have be connected to DummyService' do
+            expect(subject.service).to eq Rester::DummyService
+          end
+        end # with service class
+
+        context 'with file path' do
+          let(:service) { 'spec/stubs/dummy_stub.yml' }
+
+          it { is_expected.to be_a Rester::Client::Adapters::StubAdapter }
+          it { is_expected.to have_attributes(connected?: true) }
+
+          it 'should have be connected to correct stub file' do
+            expect(subject.stub.path).to eq service
+          end
+        end
+
+        context 'with timeout' do
+          let(:opts) { { timeout: 1234.1234 } }
+          let(:service) { DummyService }
+          it { is_expected.to have_attributes(timeout: 1234.1234) }
+        end # with timeout
+
+        context 'without timeout specified' do
+          let(:opts) { {} }
+          let(:service) { DummyService }
+          it { is_expected.to have_attributes(timeout: nil) }
+        end # without timeout specified
+      end # ::connect
     end # Adapters
   end # Client::Adapters
 end # Rester
