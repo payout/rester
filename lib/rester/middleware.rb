651ccdf202ab1881f2497b078fd88ab16a5c268b
Robert Honer <robert@ribbon.co>
2015-12-10 23:54:48 -0800

    [#32] Adding NewRelic middleware

--- lib/rester/middleware.rb
+++ lib/rester/middleware.rb
@@ -3,5 +3,6 @@ module Rester
     autoload(:Base,          'rester/middleware/base')
     autoload(:ErrorHandling, 'rester/middleware/error_handling')
     autoload(:Ping,          'rester/middleware/ping')
+    autoload(:NewRelic,      'rester/middleware/new_relic')
   end
 end
---
+++ spec/support/new_relic_test_service.rb
@@ -0,0 +1,24 @@
+require 'rester'
+
+class NewRelicTestService < Rester::Service
+  use Rester::Middleware::NewRelic
+
+  module V1
+    class MountedResource < Rester::Service::Resource
+      id :token
+
+      def search
+      end
+    end
+
+    class Test < Rester::Service::Resource
+      mount MountedResource
+
+      def search
+      end
+
+      def create
+      end
+    end
+  end
+end
